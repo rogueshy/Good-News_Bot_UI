95cd32b80cd4093fb894a91c6143881cdb78a1df
Зайцев Александр Сергеевич <aleksandr.zaycev@alfanord.ru>
2017-09-21 17:53:59 +0300

    Add project files.

---
+++ Good-News_Bot_UI/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Good_News_Bot_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            label2.Text = "Running...";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            label2.Text = "Stopped.";
+        }
+
+        private void Form1_Resize_1(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.BalloonTipText = "Still working, bitch";
+                notifyIcon1.BalloonTipTitle = "I AM THE TITLE";
+                notifyIcon1.ShowBalloonTip(500);
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
